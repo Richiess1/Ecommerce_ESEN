0aa7a7a5ebb495ca6d95976e210fff2ae9e8731d
Richie <20235894@esen.edu.sv>
2025-08-01 10:54:44 -0600

    fix: update Stripe package to version 17.7.0 and refactor webhook event handling

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,50 +1,40 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+import { stripe } from '@/lib/stripe';
+import { headers } from 'next/headers';
 import Stripe from 'stripe';
 import { updateOrderToPaid } from '@/lib/actions/order.actions';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-02-24.acacia',
-});
-
 export async function POST(req: NextRequest) {
-  const sig = req.headers.get('stripe-signature') as string;
+  const rawBody = await req.text();
+  const signature = headers().get('stripe-signature') as string;
 
-  let event;
+  let event: Stripe.Event;
 
   try {
-    const body = await req.text();
-
     event = stripe.webhooks.constructEvent(
-      body,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET as string
+      rawBody,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
     );
-  } catch (err: any) {
-    console.error('Stripe webhook error:', err.message);
-    return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
+  } catch (err) {
+    return new Response(`Webhook Error: ${err}`, { status: 400 });
   }
 
-  // Check for successful payment
   if (event.type === 'charge.succeeded') {
-    const { object } = event.data;
+    const charge = event.data.object as Stripe.Charge;
 
-    // Update order status
     await updateOrderToPaid({
-      orderId: object.metadata.orderId,
+      orderId: charge.metadata.orderId,
       paymentResult: {
-        id: object.id,
+        id: charge.id,
         status: 'COMPLETED',
-        email_address: object.billing_details.email!,
-        pricePaid: (object.amount / 100).toFixed(),
+        email_address: charge.billing_details.email!,
+        pricePaid: (charge.amount / 100).toFixed(),
       },
     });
 
-    return NextResponse.json({
-      message: 'updateOrderToPaid was successful',
-    });
+    return new Response('updateOrderToPaid was successful', { status: 200 });
   }
 
-  return NextResponse.json({
-    message: 'event is not charge.succeeded',
-  });
+  return new Response('event is not charge.succeeded', { status: 200 });
 }
